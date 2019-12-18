6e479dd9278c438f5c5f9c66b7ecf0fed892c7a4
Matt <simcrimsos@gmail.com>
2019-12-18 16:00:02 -0500

    server console print

--- src/model/server/searchEngine/Response.kt
+++ src/model/server/searchEngine/Response.kt
@@ -1,5 +1,6 @@
 package projetift604.model.server.searchEngine
 
+import SLog
 import com.google.gson.annotations.Expose
 import org.json.JSONObject
 import projetift604.user.SearchCall
@@ -7,13 +8,15 @@ import projetift604.user.User
 
 fun formatResponse(call: SearchCall, response: Response, user: User?): String {
     user!!.addLastResearch(call, response)
-    System.out.println(
-        "Response-------------" + "\n" +
-                "\t-call: ${call}" + "\n" +
-                "\t-resp: ${response.toString().subSequence(0, 150)}..." + "\n" +
-                "\t-user: ${user}" + "\n" +
-                "-------------" + "\n"
-    )
+    SLog.changeIndent(SLog.Companion.INDENT.RESET)
+    SLog.log("-------------")
+    SLog.log("Response:")
+    SLog.changeIndent(SLog.Companion.INDENT.PLUS)
+    SLog.log("call: ${call}")
+    SLog.log("resp: ${response.toString().subSequence(0, 150)}...")
+    SLog.log("user: ${user}:")
+    SLog.log("-------------")
+    SLog.changeIndent(SLog.Companion.INDENT.MINUS)
     return response.toString()
 }
 
